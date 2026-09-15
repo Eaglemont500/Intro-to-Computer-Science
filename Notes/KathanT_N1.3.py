1fac167f943f75b539f0c3cf1c64baf6cc4e85e1
Eaglemont500 <kathanjacob@gmail.com>
2026-09-15 12:16:35 -0400

    Added N1.3

---
+++ Notes/KathanT_N1.3.py
@@ -0,0 +1,38 @@
+'''
+base10 = int(input("Enter a Base 10 number to convert to Binary\n"))
+
+binary = bin(base10)
+binary = binary.removeprefix("0b")
+
+print(binary)
+'''
+
+while True:
+    try:
+        base10 = int(input("Enter a Base 10 integer to convert to Binary (between 0 and 31)\n"))
+    except ValueError:
+        print("Please enter an integer.")
+
+    except (EOFError, KeyboardInterrupt):
+        print("\nProgram terminated")
+        exit()
+
+    if base10 > 31:
+        print("Please enter a smaller number.")
+    elif base10 < 0:
+        print("Please enter a larger number.")
+    else:
+        break
+place0 = base10 % 2
+base10 = base10 // 2
+place1 = base10 % 2
+base10 = base10 // 2
+place2 = base10 % 2
+base10 = base10 // 2
+place3 = base10 % 2
+base10 = base10 // 2
+place4 = base10 % 2
+
+binary = f"{place4}{place3}{place2}{place1}{place0}"
+
+print(f"Your Base10 number in binary is {binary}.")
