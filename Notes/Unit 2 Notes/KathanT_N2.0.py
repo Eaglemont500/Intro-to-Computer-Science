7330acb143ea93a0e4b896d8d175474bb4bca206
Eaglemont500 <kathanjacob@gmail.com>
2026-09-24 12:09:59 -0400

    Refactored Notes folder to subdivide into Units, Added N2.0

---
+++ Notes/Unit 2 Notes/KathanT_N2.0.py
@@ -0,0 +1,40 @@
+## Mathmatical Operators
+x=10
+y=20
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
+
+
+## Logical Operators / Comparison Operators
+## Only returns boolean values 
+## 1 is True and 0 is False
+
+## Comparison of numbers
+print(x==y) # Are they equal?
+print(x!=y) # Are they not equal?
+
+print(x<y) # Is it less than?
+print(x>y) # Is it greater than?
+
+print(x>=y) # Is it greater than or equal to?
+print(x<=y) # Is it less than or equal to?
+
+
+
+## Assignment of Variables
+x = 2
+x = x + 1
+print(x)
+
+y = 2
+z = 1
+z += y # Same as z = z + y
+print(z)
+z -= y # Same as z = z - y
+print(z)
